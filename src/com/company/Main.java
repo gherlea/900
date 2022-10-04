db639e9dea59f5dbbf410df1502e5a8af1a10908
gherlea <101740436+gherlea@users.noreply.github.com>
2022-10-04 23:26:10 +0300

    revenire 900

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,11 +8,12 @@ public class Main {
         int x = 2;
         int y = 3;
         double z =2.5;
+        int w = 3;
+
         System.out.println(x+y);
         System.out.println(x-y);
         System.out.println(x-z);
-
-
+        System.out.println(x+w);
         System.out.println(x*y);
     }
 }
