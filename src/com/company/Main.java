31a548520059454ce0d03a7c7ea8993b00be0543
gherlea <101740436+gherlea@users.noreply.github.com>
2022-10-04 23:23:54 +0300

    TEMA LAB 900

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,14 +4,14 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("MARIA SALUT!!!!!!");
+        System.out.println("MARIA !!!!");
         int x = 2;
         int y = 3;
         double z =2.5;
         System.out.println(x+y);
         System.out.println(x-y);
         System.out.println(x-z);
-        
+
 
         System.out.println(x*y);
     }
